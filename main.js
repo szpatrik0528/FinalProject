fdd99962a79e12230548c205c99f40143b8c3752
szpatrik0528 <124908897+szpatrik0528@users.noreply.github.com>
2023-09-07 09:49:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const cart = {};
+const exchangeRate = 300; // 1 dollár ($) = 300 magyar forint (Ft)
+
+function addToCart(productId, productName, productPrice) {
+    if (!cart[productId]) {
+        cart[productId] = {
+            name: productName,
+            price: productPrice,
+            quantity: 1
+        };
+    } else {
+        cart[productId].quantity++;
+    }
+
+    updateCartDisplay();
+}
+
+function updateCartDisplay() {
+    const cartList = document.getElementById('cart');
+    const cartTotal = document.getElementById('cart-total');
+    let totalFt = 0;
+
+    cartList.innerHTML = '';
+
+    for (const productId in cart) {
+        const { name, price, quantity } = cart[productId];
+        const listItem = document.createElement('li');
+        listItem.textContent = `${name} - $${price} x ${quantity}`;
+        cartList.appendChild(listItem);
+        totalFt += price * quantity * exchangeRate;
+    }
+
+    cartTotal.textContent = totalFt.toFixed(2) + ' Ft';
+}
+
+function checkout() {
+    let totalFt = 0;
+
+    for (const productId in cart) {
+        const { price, quantity } = cart[productId];
+        totalFt += price * quantity * exchangeRate;
+    }
+
+    alert('Checkout button clicked. Cart contents: ' + JSON.stringify(cart) + '\nTotal: ' + totalFt.toFixed(0) + ' Ft');
+}
+
+function openCartPage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+    window.open('kosar.html');
+}
