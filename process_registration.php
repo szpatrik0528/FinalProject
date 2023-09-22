7c85270c4656362e736465bee25d714ed5bd9886
szpatrik0528 <szpatrik.photoshop@gmail.com>
2023-09-22 20:50:48 +0200

    main

--- process_registration.php
+++ process_registration.php
@@ -1,7 +1,32 @@
-if ($stmt->execute()) {
-    // Registration successful, redirect to the thank you page
-    header("Location: thank_you.php");
-    exit(); // Terminate the script to prevent further execution
-} else {
-    echo "Error: " . $stmt->error;
+<?php
+$servername = "localhost";
+$username = "your_db_username";
+$password = "your_db_password";
+$dbname = "Regisztralas";
+
+// Create a database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Insert data into the database
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+
+?>
