fe0d438d9c0035bdd9ee18ff7b914be5b0050011
szpatrik0528 <szpatrik.photoshop@gmail.com>
2023-09-28 10:06:39 +0200

    main

--- main.js
+++ main.js
@@ -85,4 +85,30 @@ paymentButton.addEventListener('click', function () {
     window.location.href = 'payment.html';
 });
 
+document.addEventListener('DOMContentLoaded', function(event) {
+
+    document.getElementById('flip-card-btn-turn-to-back').style.visibility = 'visible';
+    document.getElementById('flip-card-btn-turn-to-front').style.visibility = 'visible';
+  
+    document.getElementById('flip-card-btn-turn-to-back').onclick = function() {
+    document.getElementById('flip-card').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('flip-card-btn-turn-to-front').onclick = function() {
+    document.getElementById('flip-card').classList.toggle('do-flip');
+    };
+  
+  });
+ 
+
+document.getElementById('flip-card-btn-turn-to-back').style.visibility = 'visible';
+document.getElementById('flip-card-btn-turn-to-front').style.visibility = 'visible';
+
+document.getElementById('flip-card-btn-turn-to-back').onclick = function() {
+document.getElementById('flip-card').classList.toggle('do-flip');
+};
+
+document.getElementById('flip-card-btn-turn-to-front').onclick = function() {
+document.getElementById('flip-card').classList.toggle('do-flip');
+};
 
